c285c0343091179d4d7bab3da07206f8927b4ecb
Sean Rankine <sean.rankine@me.com>
2018-06-02 18:05:29 -0400

    Updated doc strings and code organisation

--- lib/md5.ex
+++ lib/md5.ex
@@ -1,19 +1,7 @@
 defmodule Md5 do
   @moduledoc """
-  Documentation for Md5.
+  Provides methods for calculating a MD5 hash. This module implements the MD5 hashing algorithm in pure Elixir.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Md5.digest("Hello world")
-      "3e25960a79dbc69b674cd4ec67a72c62"
-
-  """
-
-  # Import module to perform bitwise operations
   use Bitwise
 
   # Pre-determined constants to shift bits - aproximated to give biggest avalanche
@@ -26,28 +14,31 @@ defmodule Md5 do
 
   @buffer_preset {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476}
 
-  def overflow(value) do
-    # Use bitwise 'AND' to mask anything more than 32 bits and "emulate" an overflow
-    value &&& 0xFFFFFFFF
-  end
-
-  # Some more constants
+  @doc """
+  Used to calculate the constants of the 64-element table defined in the specification.
+  """
   def calc_constant(i) do
     trunc(:math.pow(2, 32) * abs(:math.sin(i + 1))) &&& 0xFFFFFFFF
   end
 
-  # Perform digest on message and return MD5 hex checksum
+  @doc """
+  Returns hex representation of a MD5 digest of a message of arbitrary size.
+  """
   def digest(message) do
     x = hash(message)
     Base.encode16(<<x::little-unsigned-size(128)>>, case: :lower)
   end
 
-  # Perform digest on file contents
+  @doc """
+  Returns hex representation of a MD5 digest of a file.
+  """
   def digest_file(filepath) do
     digest(File.read!(filepath))
   end
 
-  # Generate bit hash
+  @doc """
+  Returns MD5 hash of a message of arbitrary size.
+  """
   def hash(message) do
     {a, b, c, d} = @buffer_preset
 
@@ -55,13 +46,27 @@ defmodule Md5 do
     process_message(padded_message, a, b, c, d)
   end
 
-  # Base case - when no more message to process
+  # Produced padded message according to MD5 specification
+  def pad(message) do
+    msg_length = bit_size(message)
+
+    num_of_zeros = 512 - rem(msg_length + 65, 512)
+
+    # Message + 1 + (..000..)? + size of message
+    <<message::binary, 1::little-size(1), 0::little-size(num_of_zeros), msg_length::little-size(64)>>
+  end
+
+  @doc """
+  Returns the concatenated message digest when no more message left to process.
+  """
   def process_message(message, a, b, c, d) when bit_size(message) == 0 do
     # Bit shift to put 128 bits together
     a + (b <<< 32) + (c <<< 64) + (d <<< 96)
   end
 
-  # Processing the 512 bits of the message
+  @doc """
+  Computes the state change for the message digest using first 512 bits of a message.
+  """
   def process_message(message, a, b, c, d) do
     # Use pattern matching to grab first 512 bits of message
     <<chunk::bitstring-size(512), remaining::bitstring>> = message
@@ -79,26 +84,14 @@ defmodule Md5 do
     )
   end
 
-  def index_func(i, x, y, z) when i < 16 do
-    {(x &&& y) ||| (~~~x &&& z), i}
-  end
-
-  def index_func(i, x, y, z) when i < 32 do
-    {(x &&& z) ||| (y &&& ~~~z), rem(5 * i + 1, 16)}
-  end
-
-  def index_func(i, x, y, z) when i < 48 do
-    {x ^^^ y ^^^ z, rem(3 * i + 5, 16)}
-  end
-
-  def index_func(i, x, y, z) when i < 64 do
-    {y ^^^ (x ||| ~~~z), rem(7 * i, 16)}
-  end
-
-  # Base case - return when i reaches 64
+  @doc """
+  Returns the message digest after 64 steps.
+  """
   def step(i, _, a, b, c, d) when i >= 64, do: {a, b, c, d}
 
-  # Each
+  @doc """
+  Calculates part of the state change for the message digest.
+  """
   def step(i, m, a, b, c, d) when i >= 0 do
     # Get constants and perform bitwise operations
     t = calc_constant(i)
@@ -116,19 +109,47 @@ defmodule Md5 do
     step(i + 1, m, d, b_new, b, c)
   end
 
-  # Produced padded message according to MD5 specification
-  def pad(message) do
-    msg_length = bit_size(message)
-
-    num_of_zeros = 512 - rem(msg_length + 65, 512)
-
-    # Message + 1 + (..000..)? + size of message
-    <<message::binary, 1::little-size(1), 0::little-size(num_of_zeros), msg_length::little-size(64)>>
+  @doc """
+  Round 1
+  """
+  def index_func(i, x, y, z) when i < 16 do
+    {(x &&& y) ||| (~~~x &&& z), i}
   end
 
+  @doc """
+  Round 2
+  """
+  def index_func(i, x, y, z) when i < 32 do
+    {(x &&& z) ||| (y &&& ~~~z), rem(5 * i + 1, 16)}
+  end
+
+  @doc """
+  Round 3
+  """
+  def index_func(i, x, y, z) when i < 48 do
+    {x ^^^ y ^^^ z, rem(3 * i + 5, 16)}
+  end
+
+  @doc """
+  Round 1
+  """
+  def index_func(i, x, y, z) when i < 64 do
+    {y ^^^ (x ||| ~~~z), rem(7 * i, 16)}
+  end
+
+  @doc """
+  Performs a bitwise left rotation.
+  """
   def leftrotate(b, shift) do
     b_ = overflow(b)
     overflow(b_ <<< shift ||| b_ >>> (32 - shift))
+  end
+
+  @doc """
+  Emulates at 32 bit overflow on a value.
+  """
+  def overflow(value) do
+    value &&& 0xFFFFFFFF
   end
 end
 
